b69a0f71cd9ebb5cc89db7876308a86ed7b8b17a
Marcel Boersma <boersma.marcel@gmail.com>
2011-03-09 09:58:51 +0100

    The ServerAppDelegate class

--- Rolit/src/ServerAppDelegate.java
+++ Rolit/src/ServerAppDelegate.java
@@ -9,9 +9,20 @@
 public class ServerAppDelegate {
 
 	/**
+	 * This is the main function of the server application
+	 * 
+	 * You can start a server by the following commands <b>server<b> <i>port</i> <i>optional: protocol (default is protocol...)</i> 
+	 * The 
+	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		
+		//TODO Setup protocol (depends on argument)
+		
+		//TODO Create a serverSocket with port
+		
+		
 		
 
 	}
