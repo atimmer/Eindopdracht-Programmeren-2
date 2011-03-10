267fdf3df5618bc5f688ae1de70dd9850aaf75bb
Marcel Boersma <boersma.marcel@gmail.com>
2011-03-10 15:27:38 +0100

    protocol v1 release

--- Rolit/src/server/Game.java
+++ Rolit/src/server/Game.java
@@ -1,5 +1,27 @@
 package server;
 
-public class Game {
+
+import Protocol.DataObjectHandler;
+import Protocol.DataObject.AbstractDataObject;
+
+public class Game implements DataObjectHandler{
+
+	@Override
+	public void exec(AbstractDataObject obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addProtocolReader(Object obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getIdentifier() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- Rolit/src/server/Player.java
+++ Rolit/src/server/Player.java
@@ -1,24 +1,59 @@
 package server;
+import java.net.Socket;
+import java.util.Observable;
+import java.util.Observer;
+
 import Protocol.*;
+import Protocol.DataObject.AbstractDataObject;
 
-public class Player {
+public class Player implements Observer, DataObjectHandler{
+	
+	private Object parent 	= null;
+	private Socket sock 	= null;
+	private Protocol prot 	= null;
 
-	public Player()
+	
+	public Player(Socket s, Protocol p, Object pa)
 	{
+		this.parent = pa;
+		this.sock 	= s;
+		this.prot	= p;
+	
+		//TODO create new thread for the player to listen to the socket
+		ServerController.sharedApplication().addToSocketThread(this.sock);
+		
 		
 	}
 	
-	public Player(ProtocolRolit p)
+	public void setParent(Object p)
 	{
-		ServerAppDelegate SAD = ServerAppDelegate.sharedApplication();
-		SAD.game(this, 3);
-		
-		
-		DataObject d = ProtocolRolit(new BufferedReader(New ConnectionInputReader(socket.in)));
-		
-		
-		
+		if(p != null)
+			this.parent = p;
+	}
+
+	@Override
+	public void update(Observable arg0, Object arg1) {
+		// TODO Auto-generated method stub
 		
 	}
+
+	@Override
+	public void exec(AbstractDataObject obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addProtocolReader(Object obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getIdentifier() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 	
 }
